981d6d580fa9cd4b435400306ca179989d50f5cb
shurikenmaster26 <magnuschase2607@gmail.com>
2024-05-18 09:20:21 +0530

    added the review test functionality

--- Nav/WebDesigns/my-project/src/App.jsx
+++ Nav/WebDesigns/my-project/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import logo from './Untitled.jpeg';
 
 const Quiz = () => {
-  // Define the state for storing the current question index, whether the answer is correct, and the score
+  // Define the state for storing the current question index, whether the answer is correct, the score, user responses, and review mode
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [isCorrect, setIsCorrect] = useState(null);
   const [score, setScore] = useState(0);
+  const [responses, setResponses] = useState([]);
+  const [reviewMode, setReviewMode] = useState(false);
 
   // Define the quiz questions and options
   const questions = [
@@ -72,6 +74,10 @@ const Quiz = () => {
     if (isAnswerCorrect) {
       setScore(score + 1);
     }
+    // Store the response
+    const newResponses = [...responses];
+    newResponses[currentQuestion] = { question: questions[currentQuestion].question, selectedOption, isCorrect: isAnswerCorrect };
+    setResponses(newResponses);
   };
 
   // Function to move to the next question
@@ -85,6 +91,20 @@ const Quiz = () => {
     }
     // Reset isCorrect state variable
     setIsCorrect(null);
+  };
+
+  // Function to restart the quiz
+  const restartQuiz = () => {
+    setCurrentQuestion(0);
+    setIsCorrect(null);
+    setScore(0);
+    setResponses([]);
+    setReviewMode(false);
+  };
+
+  // Function to toggle review mode
+  const toggleReviewMode = () => {
+    setReviewMode(!reviewMode);
   };
 
   // Render the current question and options
@@ -103,53 +123,77 @@ const Quiz = () => {
 
       {/* Quiz Content */}
       <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
-        {currentQuestion < questions.length ? (
+        {reviewMode ? (
           <div>
-            <h1 className="text-2xl font-bold mb-4">Question {currentQuestion + 1}</h1>
-            <h2 className="text-lg mb-5">{questions[currentQuestion].question}</h2>
-            {/* Display message if the answer is correct */}
-            {isCorrect !== null && (
-              <div className={`mb-4 text-center ${isCorrect ? 'text-green-500' : 'text-red-500'}`}>
-                {isCorrect ? 'Correct!' : 'Incorrect!'}
+            <h1 className="text-2xl font-bold mb-4">Review Your Mistakes</h1>
+            {responses.map((response, index) => (
+              <div key={index} className="mb-4">
+                <h2 className="text-lg mb-2">Question {index + 1}: {response.question}</h2>
+                <p className={`mb-2 ${response.isCorrect ? 'text-green-500' : 'text-red-500'}`}>
+                  Your answer: {response.selectedOption} - {response.isCorrect ? 'Correct' : 'Incorrect'}
+                </p>
               </div>
-            )}
-            {/* Display options */}
-            <div className="grid grid-cols-2 gap-4">
-              {questions[currentQuestion].options.map((option, index) => (
-                <button 
-                  key={index} 
-                  className={`block w-full py-2 px-4 mb-2 rounded-full ${isCorrect && questions[currentQuestion].answer === option ? 'bg-green-500 text-white' : 'bg-blue-500 text-white'} hover:bg-blue-900 focus:outline-none focus:bg-blue-600`}
-                  onClick={() => handleOptionSelect(option)}
-                  disabled={isCorrect !== null}
-                >
-                  {option}
-                </button>
-              ))}
-            </div>
-            {/* Display next question button */}
+            ))}
             <button 
               className="block w-full py-2 px-4 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-900 focus:outline-none focus:bg-blue-600"
-              onClick={moveToNextQuestion}
-              disabled={isCorrect === null}
-            >
-              Next Question
-            </button>
-          </div>
-        ) : (
-          <div className="text-center">
-            <h1 className="text-2xl font-bold mb-4">Quiz Finished!</h1>
-            <p className="text-lg mb-5">Congratulations! You have completed the quiz.</p>
-            <p className="text-lg mb-5">Your score is: {score} out of {questions.length}</p>
-            <button 
-              className="block w-full py-2 px-4 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-900 focus:outline-none focus:bg-blue-600"
-              onClick={() => {
-                setCurrentQuestion(0);
-                setIsCorrect(null);
-                setScore(0); // Reset the score
-              }}
+              onClick={restartQuiz}
             >
               Restart Quiz
             </button>
+          </div>
+        ) : (
+          <div>
+            {currentQuestion < questions.length ? (
+              <div>
+                <h1 className="text-2xl font-bold mb-4">Question {currentQuestion + 1}</h1>
+                <h2 className="text-lg mb-5">{questions[currentQuestion].question}</h2>
+                {/* Display message if the answer is correct */}
+                {isCorrect !== null && (
+                  <div className={`mb-4 text-center ${isCorrect ? 'text-green-500' : 'text-red-500'}`}>
+                    {isCorrect ? 'Correct!' : 'Incorrect!'}
+                  </div>
+                )}
+                {/* Display options */}
+                <div className="grid grid-cols-2 gap-4">
+                  {questions[currentQuestion].options.map((option, index) => (
+                    <button 
+                      key={index} 
+                      className={`block w-full py-2 px-4 mb-2 rounded-full ${isCorrect && questions[currentQuestion].answer === option ? 'bg-green-500 text-white' : 'bg-blue-500 text-white'} hover:bg-blue-900 focus:outline-none focus:bg-blue-600`}
+                      onClick={() => handleOptionSelect(option)}
+                      disabled={isCorrect !== null}
+                    >
+                      {option}
+                    </button>
+                  ))}
+                </div>
+                {/* Display next question button */}
+                <button 
+                  className="block w-full py-2 px-4 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-900 focus:outline-none focus:bg-blue-600"
+                  onClick={moveToNextQuestion}
+                  disabled={isCorrect === null}
+                >
+                  Next Question
+                </button>
+              </div>
+            ) : (
+              <div className="text-center">
+                <h1 className="text-2xl font-bold mb-4">Quiz Finished!</h1>
+                <p className="text-lg mb-5">Congratulations! You have completed the quiz.</p>
+                <p className="text-lg mb-5">Your score is: {score} out of {questions.length}</p>
+                <button 
+                  className="block w-full py-2 px-4 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-900 focus:outline-none focus:bg-blue-600"
+                  onClick={restartQuiz}
+                >
+                  Restart Quiz
+                </button>
+                <button 
+                  className="block w-full py-2 px-4 mt-4 bg-blue-500 text-white rounded-md hover:bg-blue-900 focus:outline-none focus:bg-blue-600"
+                  onClick={toggleReviewMode}
+                >
+                  Review Your Mistakes
+                </button>
+              </div>
+            )}
           </div>
         )}
       </div>
